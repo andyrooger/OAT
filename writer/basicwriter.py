93e8fe2453d19879a37c67a16271ea99443555bf
Andy <as.gurden@gmail.com>
2011-01-19 00:45:31 +0000

    Added file ready for a basic source writer

---
+++ writer/basicwriter.py
@@ -0,0 +1,7 @@
+"""
+Holds the base class for source writers, it should somehow take an AST and
+ouput correct source.
+
+This should be extended for obfuscated syntax or really pretty output etc.
+"""
+
