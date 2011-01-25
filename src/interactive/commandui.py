9436fa47a96dc22e29250640502d30c840ec6158
Andy <as.gurden@gmail.com>
2011-01-25 14:36:15 +0000

    Added path completer

--- src/interactive/commandui.py
+++ src/interactive/commandui.py
@@ -6,7 +6,7 @@ Command based UI for the obfuscator.
 """
 
 import cmd
-
+import os
 
 class CommandUI(cmd.Cmd):
     """Command UI class, use self.cmdloop() to run."""
@@ -29,3 +29,37 @@ class CommandUI(cmd.Cmd):
 
         print()
         return True
+
+
+    def path_completer(self,
+                       path : "Full path",
+                       suffix : "Length of suffix to replace/extend"):
+        """Completer for file paths."""
+
+        directory, base = os.path.split(path)
+        fixed = path[:-suffix] if suffix else path
+        fixed_l = len(fixed)
+        entries = []
+
+        try:
+            if directory:
+                entries = os.listdir(directory)
+            else:
+                entries = os.listdir(os.getcwd())
+        except OSError:
+            entries = []
+
+        suggestions = [os.path.join(directory, file) for file in entries if file.startswith(base)]
+
+        replaceables = [file[fixed_l:] for file in suggestions if file.startswith(fixed)]
+
+        return replaceables
+
+    
+    def do_parse(self, line):
+        """Parse a source file to an AST."""
+
+        pass
+
+    def complete_parse(self, text, line, begidx, endidx):
+        return self.path_completer(line.rpartition(" ")[2], len(text))
