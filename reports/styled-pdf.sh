816219f6b7631a9cf0cb688597b6a613744ef4ff
Andy <as.gurden@gmail.com>
2011-02-07 00:54:21 +0000

    Added script for pdf translation in my style

---
+++ reports/styled-pdf.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm -rf temp > /dev/null
+mkdir temp > /dev/null
+cd temp
+
+BASEFILE=${1%\.*}
+
+pandoc -t latex --template=../report.template --base-header-level=2 -o $BASEFILE.tex ../$1 &&
+pdflatex $BASEFILE.tex > /dev/null
+
+cp $BASEFILE.pdf ..
+cd ..
+rm -rf temp
+
