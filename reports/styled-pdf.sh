fefe4191f98f8e5c12821b2eecd62f24dd6a9d6d
Andy <as.gurden@gmail.com>
2011-02-11 14:01:23 +0000

    Updated compiler script to fix pstricks death

--- reports/styled-pdf.sh
+++ reports/styled-pdf.sh
@@ -1,19 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -rf temp > /dev/null
-mkdir temp > /dev/null
-cd temp
+echo -n Setup...
+rm -rf temp &> /dev/null
+mkdir temp &> /dev/null
+cd temp &> /dev/null
 
 BASEFILE=${1%\.*}
 
-cp ../$BASEFILE.tex .
-cp ../$BASEFILE.bib .
-pdflatex $BASEFILE.tex > /dev/null
-bibtex $BASEFILE.aux > /dev/null
-pdflatex $BASEFILE.tex > /dev/null
-pdflatex $BASEFILE.tex > /dev/null
+cp ../$BASEFILE.tex . &> /dev/null
+cp ../$BASEFILE.bib . &> /dev/null
+
+echo -n Compilation...
+
+latex $BASEFILE.tex &> /dev/null
+bibtex $BASEFILE.aux &> $BASEFILE.bib.log
+latex $BASEFILE.tex &> /dev/null
+latex $BASEFILE.tex &> $BASEFILE.tex.log
+
+echo -n Conversion...
+
+dvips $BASEFILE.dvi &> /dev/null
+ps2pdf $BASEFILE.ps &> /dev/null
+
+echo -n Cleanup...
 
 cp $BASEFILE.pdf ..
+cp $BASEFILE.bib.log ..
+cp $BASEFILE.tex.log ..
 cd ..
 rm -rf temp
 
+echo Done
