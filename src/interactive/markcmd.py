6447fe790e780288f94b784aa0c424aeb7e8c754
Andy <as.gurden@gmail.com>
2011-03-15 19:45:12 +0000

    Added read/write markings

--- src/interactive/markcmd.py
+++ src/interactive/markcmd.py
@@ -17,6 +17,10 @@ class MarkCommand(commandui.Command):
                                 help="Is this a visible or invisible statement? (i.e. does it perform actions visible to the outside world?")
         self._opts.add_argument("-b", "--breaks", choices=["yes", "no"],
                                 help="Can this statement break from a linear flow?")
+        self._opts.add_argument("-r", "--reads", nargs="+", metavar="var",
+                                help="Choose all the variables this node reads. Use (+|-)(g|n|l|u)variable to add or remove a global, nonlocal, local, or unknown variable.")
+        self._opts.add_argument("-w", "--writes", nargs="+", metavar="var",
+                                help="Choose all the variables this node writes. Use (a|r)(g|n|l|u)variable to add or remove a global, nonlocal, local, or unknown variable.")
 
         self._related_parsecmd = parsecmd
         self._related_explorecmd = explorecmd
@@ -38,6 +42,11 @@ class MarkCommand(commandui.Command):
             for mark in newmarkings
             if newmarkings[mark] != None
         }
+
+        if "reads" in newmarkings:
+            newmarkings["reads"] = self.parseRefs(newmarkings["reads"], "reads")
+        if "writes" in newmarkings:
+            newmarkings["writes"] = self.parseRefs(newmarkings["writes"], "writes")
 
         if newmarkings:
             self._update_markings(newmarkings)
@@ -77,3 +86,59 @@ class MarkCommand(commandui.Command):
 
         if marks:
             self._related_parsecmd.ast.augmented = True
+
+
+    def parseRefs(self, changes, mark):
+        """
+        Parse the function references from the command line.
+
+        Uses format (a|r)(g|n|l|u)variable.
+
+        """
+
+        self._related_explorecmd._ensure_node_sync()
+        try:
+            refs = self._get_markings()[mark].copy()
+        except KeyError:
+            refs = {
+                "global": set(),
+                "nonlocal": set(),
+                "local": set(),
+                "unknown": set()
+            }
+
+        for change in changes:
+            if len(change) < 3:
+                print("Unrecognised argument: " + change)
+                continue
+
+            try:
+                category = {
+                    "g": "global",
+                    "n": "nonlocal",
+                    "l": "local",
+                    "u": "unknown",
+                }[change[1]]
+            except KeyError:
+                print("Unknown type: " + change[1] + " in " + change)
+                continue
+
+            try:
+                do = {
+                    "a" : refs[category].add,   # Categories are created with
+                    "r" : refs[category].remove # the original marking
+                }[change[0]]
+            except KeyError:
+                print("Invalid action: " + change[0] + " in " + change)
+                continue
+
+
+            variable = change[2:]
+
+            try:
+                do(variable)
+            except KeyError:
+                pass # Means we tried to remove a variable that didn't exist
+            
+
+        return refs
