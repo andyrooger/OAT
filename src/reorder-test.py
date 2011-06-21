a9b5fdc75d70967841edb3477f62bc224aeb7c08
Andy <as.gurden@gmail.com>
2011-06-21 09:45:44 +0100

    Added a testing function to create csv of results

---
+++ src/reorder-test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from analysis.reorder import WriteUseValuer
+
+def test(size, visible, breaking, rprob, wprob, rand, valuer):
+    for testno in range(5):
+        stats = create_statements(size, visible=visible, breaking=breaking, rprob=rprob, wprob=wprob)
+        #printSource(stats, PrettyWriter)
+        time = timed_reorder(stats, timeout, rand, valuer)
+        print(",".join(str(x) for x in [size,visible,breaking,rprob,wprob,rand,valuer.__class__.__name__,time]))
+
+if __name__ == "__main__":
+    from writer.prettywriter import PrettyWriter
+    from writer.sourcewriter import printSource
+    from testing.reorder import *
+
+    timeout = 30*60
+
+    print("size,visible,breaking,rprob,wprob,rand,valuer,testno,time")
+
+    for i in range(1, 11):
+        for rprob in [0.1, 0.5, 0.9]:
+            for wprob in [0.1, 0.5, 0.9]:
+                for visible in [0.1, 0.5]:
+                    for breaks in [0.1, 0.5]:
+                        for rand in [True, False]:
+                            for valuer in [None, WriteUseValuer]:
+                                test(i, visible, breaks, rprob, wprob, rand, valuer)
