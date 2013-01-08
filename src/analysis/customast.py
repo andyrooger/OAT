564b74fcd17b04f024118c4001e929734eea0581
Andy Gurden <as.gurden@gmail.com>
2013-01-08 21:08:06 +0000

    Added a license, don't know why I'm only just doing this. Argparse requires a license as it is under Apache v2...

--- src/analysis/customast.py
+++ src/analysis/customast.py
@@ -5,6 +5,24 @@ This should provide extra functionality to make certain things simpler.
 
 """
 
+# OAT - Obfuscation and Analysis Tool
+# Copyright (C) 2011  Andy Gurden
+# 
+#     This file is part of OAT.
+# 
+#     OAT is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     OAT is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with OAT.  If not, see <http://www.gnu.org/licenses/>.
+
 import ast
 
 class CustomAST:
